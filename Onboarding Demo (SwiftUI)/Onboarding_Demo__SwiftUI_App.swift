e51a3768bd8854cc6f40669e2d2b29f5e8c9329f
Trevor <bugdent@gmail.com>
2022-10-31 14:04:58 -0700

    Initial Commit

---
+++ Onboarding Demo (SwiftUI)/Onboarding_Demo__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_Demo__SwiftUI_App.swift
+//  Onboarding Demo (SwiftUI)
+//
+//  Created by Jesse Chan on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_Demo__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
